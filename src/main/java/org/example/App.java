cc8f121fca39a03d3d909557936b5f7eaf1e0307
mike <mike@mikes-MacBook-Pro-2.local>
2021-07-21 21:37:50 -0700

    created branch > test-branch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,21 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
 public class App 
 {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static java.util.logging.Logger logger2 = java.util.logging.Logger.getLogger(App.class.getName());
+
     public static void main( String[] args )  {
-        System.out.println("Hello world");
+        logger.info("slf4j logger");
+        logger2.info("logger2 - java.util.logging.Logger");
+        log.info("hello world");
+
         App app = new App();
         app.testMethod1();
         testMethod2();
@@ -16,7 +24,6 @@ public class App
     public void testMethod1() {
         System.out.println("this is a test method");
     }
-
 
     public static final void testMethod2() {
         System.out.println("this is a static test method");
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,19 +2,27 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Unit test for simple App.
- */
+import java.util.logging.Level;
+
+@Slf4j
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private static Logger logger = LoggerFactory.getLogger(AppTest.class);
+    private static java.util.logging.Logger logger2 = java.util.logging.Logger.getLogger(AppTest.class.getName());
+
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        log.info("slf4j logger from lombok");
+
+        logger.info("slf4j logger from private static logger instantiated");
+
+        logger2.setLevel(Level.WARNING);
+        logger2.info("logger2 from java.util.logging");
     }
 }
